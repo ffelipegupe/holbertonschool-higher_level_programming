49adeab4200fe37f01f5e4464199cd68591940d2
ffelipegupe <1560@holbertonschool.com>
2020-04-28 18:48:51 -0500

    [Editing] Refactor

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for n in range(0, 100):
-    if n < 99:
+    if n != 99:
         print("{:02d}, ".format(n), end ="")
-    elif n == 99:
+    else:
         print("{:02d}".format(n))
