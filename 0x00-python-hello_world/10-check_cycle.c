dd08c2d9012fc7ba02f2be58470fb7d7ef9470db
ffelipegupe <1560@holbertonschool.com>
2020-04-28 00:23:37 +0000

    [Add] Adding 10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * check_cucle - function that checks if a singly linked list has a cycle
+ * @list: list to check
+ *
+ * Return: 0, if the is no cycle; 1, if there is a cycle
+ **/
+int check_cycle(listint_t *list)
+{
+	listint_t *back = list;
+	listint_t *forth = list;
+
+	while (back && forth && forth->next)
+	{
+		if (back == forth)
+			return (1);
+		back = back->next;
+		forth = forth->next->next;
+	}
+	return (0);
+}
