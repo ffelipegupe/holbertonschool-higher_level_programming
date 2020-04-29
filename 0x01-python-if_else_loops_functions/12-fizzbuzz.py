c367f17d2df326836b9ec0e788423412d64e3b55
ffelipegupe <1560@holbertonschool.com>
2020-04-28 21:08:27 -0500

    [Edit] Editing function

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,7 +2,7 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end="")
+            print("FizzBuzz", end=" ")
         elif i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
