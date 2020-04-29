1ec71217c7c5929a6f0f12532e963b6d464eeb18
ffelipegupe <1560@holbertonschool.com>
2020-04-29 03:29:56 +0000

    [Add] Adding 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,6 +14,8 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
 	listint_t *low;
 
+	if (head == NULL)
+		return (NULL);
 	if (*head == NULL)
 	{
 		new = add_nodeint_end(head, number);
