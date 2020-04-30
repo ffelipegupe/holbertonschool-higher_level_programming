355b606f4ba7c0e5db9e795831aa7d1f0b5a031a
ffelipegupe <1560@holbertonschool.com>
2020-04-30 18:49:45 -0500

    [Edit] Correcting PEP8

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,21 +8,23 @@ def main():
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     elif len(sys.argv) == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
         ope = ['+', '-', '*', '/']
         if sys.argv[2] not in ope:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
         elif sys.argv[2] == '+':
-            sum = add(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], sum))
+            sum = add(a,b)
+            print("{} {} {} = {}".format(a, sys.argv[2], b, sum))
         elif sys.argv[2] == '-':
-            res = sub(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], res))
+            res = sub(a, b)
+            print("{} {} {} = {}".format(a, sys.argv[2], b, res))
         elif sys.argv[2] == '*':
             mu = mul(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], mu))
+            print("{} {} {} = {}".format(a, sys.argv[2], b, mu))
         elif sys.argv[2] == '/':
             di = div(int(sys.argv[1]), int(sys.argv[3]))
-            print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], di))
+            print("{} {} {} = {}".format(a, sys.argv[2], b, di))
 if __name__ == "__main__":
         main()
