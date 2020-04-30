cb20b2973637df68912727e7f61a923df194fd09
ffelipegupe <1560@holbertonschool.com>
2020-04-30 12:31:23 -0500

    [Edit] Editing guard

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 from add_0 import add
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+def main():
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 if __name__ == "__main__":
-    import sys
-    add(int(sys.arg[2]))
+    main()
