6c38c7f55c69340e1c72bd0242ab3a570c19ba1e
ffelipegupe <1560@holbertonschool.com>
2020-04-30 18:30:37 -0500

    [Edit] Correcting style

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -24,6 +24,5 @@ def main():
         elif sys.argv[2] == '/':
             di = div(int(sys.argv[1]), int(sys.argv[3]))
             print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], di))
-
 if __name__ == "__main__":
         main()
