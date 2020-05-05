b5fdf3fce99dfdec1ab5808d1d6034934f14508c
ffelipegupe <1560@holbertonschool.com>
2020-05-05 15:01:43 -0500

    Update: requirements

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list) - 1:
+        return my_list
     for i in my_list:
         my_list.pop(idx)
         my_list.insert(idx, element)
