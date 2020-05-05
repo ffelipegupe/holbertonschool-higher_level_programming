25c9d0fab03fa9376b18e178e92312f8cd476abc
ffelipegupe <1560@holbertonschool.com>
2020-05-05 18:39:16 -0500

    Add: adding 6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print()
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print()
