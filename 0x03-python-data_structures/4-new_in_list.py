f6a0ccdecc6911546760d9a0a8fc93595191c19d
ffelipegupe <1560@holbertonschool.com>
2020-05-05 20:45:10 +0000

    Edit: erasing tests

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -7,10 +7,3 @@ def new_in_list(my_list, idx, element):
         new_list.pop(idx)
         new_list.insert(idx, element)
         return new_list
-
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
-print(new_list)
-print(my_list)
