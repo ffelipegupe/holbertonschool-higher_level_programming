8f9c8b543608535476a9e77c3c4842ccdd04045c
ffelipegupe <1560@holbertonschool.com>
2020-05-05 21:26:33 +0000

    Update: styling and mode

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -8,16 +8,3 @@ def no_c(my_string):
         l_string.remove("C")
     str = str.join(l_string)
     return str
-
-#my_string = "C is fun"
-#l = list(my_string)
-#print("c" in l)
-#l.remove("c")
-#l.remove("C")
-#s = ""
-#s = s.join(l)
-#print(my_string)
-#print(s)
-print(no_c("Holberton School"))
-print(no_c("Chicago"))
-print(no_c("C is fun!"))
