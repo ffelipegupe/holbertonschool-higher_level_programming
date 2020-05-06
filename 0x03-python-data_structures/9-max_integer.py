803aa6a8741302027639b2a482336087780a1aee
ffelipegupe <1560@holbertonschool.com>
2020-05-05 20:46:32 -0500

    Edit: erasing tests

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -6,10 +6,3 @@ def max_integer(my_list=[]):
             if x > max:
                 max = x
         return max
-
-
-my_list = [1, 2, 3, 90, 34, 5, -13, 40]
-max_value = max_integer(my_list)
-min = max_integer()
-print("Max: {}".format(max_value))
-print(min)
