eaaf817c90bb119ccc54ab66e297068e11cf20de
ffelipegupe <1560@holbertonschool.com>
2020-05-05 20:37:55 -0500

    Add: adding 9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    max = my_list[0]
+    for x in my_list:
+        if x > max:
+            max = x
+    return max
+
+
+my_list = [1, 2, 1, 34, 5, -13, 40]
+max_value = max_integer(my_list)
+print("Max: {}".format(max_value))
