b7e61669cf60a15485f108994142520c051cb1d0
ffelipegupe <1560@holbertonschool.com>
2020-05-05 20:44:53 -0500

    Update: including if statement

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = my_list[0]
-    for x in my_list:
-        if x > max:
-            max = x
-    return max
+    if my_list:
+        max = my_list[0]
+        for x in my_list:
+            if x > max:
+                max = x
+        return max
 
 
 my_list = [1, 2, 3, 90, 34, 5, -13, 40]
 max_value = max_integer(my_list)
+min = max_integer()
 print("Max: {}".format(max_value))
+print(min)
