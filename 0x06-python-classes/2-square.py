42893e4fcb5a544bbaa9cf02054055fb069b7b3f
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-19 17:34:03 -0500

    Add: adding 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+class Square:
+    '''Square class with an attribute'''
+    def __init__(self, size=0):
+        '''Private attribute size'''
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
