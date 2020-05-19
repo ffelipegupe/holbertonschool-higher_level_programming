560ecc271b540e0f4e38933ed2f5f9f9659df7bf
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-19 17:24:42 -0500

    Add: adding 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Square class'''
+
+
+class Square:
+    '''Square class with an attribute'''
+    def __init__(self, size=0):
+        '''Private attribute size'''
+        self.__size = size
+    
