ffea20fee7093dfbbef59a729bfa871e9e324eb7
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-19 17:54:05 -0500

    Edit: pep8

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,8 +7,8 @@ class Square:
     def __init__(self, size=0):
         '''Private attribute size'''
         self.__size = size
-        
-    @property        
+
+    @property
     def size(self):
         '''size getter'''
         return self.__size
@@ -25,15 +25,3 @@ class Square:
     def area(self):
         '''Instance method that returns the area of an Square'''
         return self.__size ** 2
-
-my_square = Square(89)
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-my_square.size = 3
-print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-
-try:
-    my_square.size = "5 feet"
-    print("Area: {} for size: {}".format(my_square.area(), my_square.size))
-except Exception as e:
-    print(e)
