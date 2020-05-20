628044a8eae7d2c0f361e0fa58c8e451cc495e5c
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-19 20:13:29 -0500

    Edit: exception msg

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -9,7 +9,7 @@ class MagicClass:
         '''Circle class instantiation'''
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
-            raise TypeError("radius must be a number")
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
