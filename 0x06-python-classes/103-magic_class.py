4b6862d24a78510fb2a88c87fe24a9d1123cb223
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-19 20:08:49 -0500

    Edit: docuString

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import math
 '''Library math'''
+import math
 
 
 class MagicClass:
     '''Circle class'''
-    def __init__(self, radius=0):
+    def __init__(self, radius):
         '''Circle class instantiation'''
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
@@ -14,8 +14,8 @@ class MagicClass:
 
     def area(self):
         '''Method that returns the area'''
-        return (self.__radius ** 2 * math.pi)
+        return self.__radius ** 2 * math.pi
 
     def circumference(self):
         '''Method that returns the circumference'''
-        return (2 * math.pi * self.__radius)
+        return 2 * math.pi * self.__radius
