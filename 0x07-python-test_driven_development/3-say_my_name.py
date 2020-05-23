c31b60041b4f2211db5d329f6fa2e23d1885344b
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-22 19:53:27 -0500

    Update: main

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -11,3 +11,7 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
     print("My name is {:} {:}".format(first_name, last_name))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testfile("./tests/3-say_my_name.txt")
