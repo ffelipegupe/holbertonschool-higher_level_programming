771bf6c382198a581810df8d8caba8ecc35436a6
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-25 18:55:00 -0500

    Edit: space format

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -83,7 +83,7 @@ class Rectangle:
         """String representation of a rectangle using eval()"""
         str_w = str(self.__width)
         str_h = str(self.__height)
-        return "Rectangle(" + str_w + "," + str_h + ")"
+        return "Rectangle(" + str_w + ", " + str_h + ")"
 
     def __del__(self):
         """del method"""
