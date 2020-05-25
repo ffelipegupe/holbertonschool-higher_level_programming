6e2718bdcb73382a8556e2605355c173aa2bf690
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-25 13:43:21 -0500

    Add: adding 1-rectangle.py

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""Rectangle Class source"""
+
+
+class Rectangle:
+    """Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """Instantiation with optional width and height"""
+        self.__height = height
+        self.__width = width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @width.setter
+    def width(self, value):
+        """width value setter"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
