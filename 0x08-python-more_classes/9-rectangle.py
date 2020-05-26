9102cdcb10e9b24cb4c3f2eeb565c94bf637785e
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-25 19:06:13 -0500

    Change: attribute

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Instantiation with optional width and height"""
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @staticmethod
