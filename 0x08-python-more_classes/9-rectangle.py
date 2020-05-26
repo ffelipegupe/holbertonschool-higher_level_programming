7c92ca9f34b01ebdbbee7d8b4f599db004aa59fd
Felipe Gutierrez <1560@holbertonschool.com>
2020-05-25 19:36:12 -0500

    Edit: layout

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -13,17 +13,6 @@ class Rectangle:
         self.width = width
         type(self).number_of_instances += 1
 
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """Method that returns the biggest rectangle based on the area"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        return rect_2
-
     @property
     def height(self):
         """height getter"""
@@ -80,6 +69,17 @@ class Rectangle:
         """del method"""
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """Method that returns the biggest rectangle based on the area"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
 
     @classmethod
     def square(cls, size=0):
