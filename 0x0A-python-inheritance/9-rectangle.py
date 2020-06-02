d71f5cdaf32effaa7308d6594fdff6fc2b6984fa
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-02 18:19:19 -0500

    Add: adding 9-rectangle.py

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Rectangle class from BaseGeometry class'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''BaseGeometry subclass'''
+    def __init__(self, width, height):
+        '''Instantiation'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''Returns the area of a rectangle'''
+        return self.__width * self.__height
+    
+    def __str__(self):
+        '''Returns a string represenation'''
+        return "[Rectangle]" + str(self.__width) + "/" + str(self.__height)
