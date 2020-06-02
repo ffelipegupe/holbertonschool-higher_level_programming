0b247af4b13fcc6249e90aba4057e715dc0db15e
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-02 18:34:23 -0500

    Add: adding 10-square.py

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''Square class from Rectangle class'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square subclass'''
+    def __init__(self, size):
+        '''Instantiation'''
+        super().integer_validation("size", size)
+        super().__init__(size, size)
