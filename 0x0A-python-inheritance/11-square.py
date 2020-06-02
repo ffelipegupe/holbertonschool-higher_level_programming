368a0ba1b8020a136237b5c3e80dab4e23603e97
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-02 18:50:57 -0500

    Add: adding 11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Square class from Rectangle class'''
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''Square subclass'''
+    def __init__(self, size):
+        '''Instantiation'''
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        '''Returns a string representation'''
+        return "[Square] " + str(self.__size) + "/" + str(self.__size)
