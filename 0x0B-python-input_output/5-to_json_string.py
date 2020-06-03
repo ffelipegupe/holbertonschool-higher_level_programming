41569e9052f9e9b80c373a0d078c56f6da2c6595
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 17:35:56 -0500

    Add: adding 5-to_json_string.py

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""Importing json module
+"""
+
+
+def to_json_string(my_obj):
+    """Function that returns the JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
