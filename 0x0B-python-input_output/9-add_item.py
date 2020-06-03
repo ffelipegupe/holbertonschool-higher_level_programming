1602cdcf618806d1479d68f6735606d106e8d7e4
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 18:27:49 -0500

    Update: functions

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,13 +4,13 @@
 
 import sys
 
-save = __import__('7-save_to_json_file').save_to_json_file
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
-load = __import__('8-load_from_json_file').load_from_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 try:
-    a_list = load('add_item.json')
+    a_list = load_from_json_file('add_item.json')
     a_list.extend(sys.argv[1:])
-    save(a_list, 'add_item.json')
+    save_to_json_file(a_list, 'add_item.json')
 
 except FileNotFoundError:
-    save(sys.argv[1:], 'add_item.json')
+    save_to_json_file(sys.argv[1:], 'add_item.json')
