65d91021e354b4f27ee74c24a09648be878f5dae
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 17:11:36 -0500

    Add: adding 2-read_lines.py

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""read_lines module
+"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """Function that reads n lines of a text file and prints
+        it to the stdout
+    """
+    with open(filename, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        print(len(lines))
+        if nb_lines <= 0 or nb_lines >= len(lines):
+            for line in lines:
+                print("{}".format(line), end="")
+        else:
+            for line in lines[:nb_lines]:
+                print("{}".format(line), end="")
