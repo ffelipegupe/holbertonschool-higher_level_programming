262861507a0e05c40bb1a83b94bd6e03dca38614
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 18:46:17 -0500

    Edit: white spaces

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,7 +11,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-    
+
     def to_json(self):
         """Public method that retrieces a dictionary representation
             of a Student instance
