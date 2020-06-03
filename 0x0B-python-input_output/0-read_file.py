bff2013c884321751e765ea28807a5a4278866a3
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 16:13:32 -0500

    Edit: erasing loop

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     """Function that reads a text file and prints it to stdout
     """
     with open(filename, encoding="utf-8") as f:
-        for line in f:
-            print(f.read(), end="")
+        print(f.read(), end="")
