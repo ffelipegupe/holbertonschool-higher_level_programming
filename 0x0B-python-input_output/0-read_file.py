b62a98bf42265f9b496b92eff4c8e17846aaf862
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 16:12:21 -0500

    Update: UTF-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@
 def read_file(filename=""):
     """Function that reads a text file and prints it to stdout
     """
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         for line in f:
-            print(line, end="")
+            print(f.read(), end="")
