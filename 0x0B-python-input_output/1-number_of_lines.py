3aaee55c278da0bcb60ae5658bd18edbd0e2c49d
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-03 16:21:19 -0500

    Update: open parameters

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -7,7 +7,7 @@ def number_of_lines(filename=""):
     """Function that returns the number of lines of a text file
     """
     lines = 0
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding='utf-8') as f:
         for line in f:
             lines += 1
     return lines
