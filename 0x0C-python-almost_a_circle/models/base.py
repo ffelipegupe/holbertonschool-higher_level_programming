679e0cc83682eae104ca254f459bb3efd292b2eb
Felipe Gutierrez <1560@holbertonschool.com>
2020-06-09 14:44:11 -0500

    Update: from_json_string return

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -44,8 +44,7 @@ class Base:
         """
         if json_string is None or len(json_string) == 0:
             return []
-        else:
-            return json.dumps(json_string)
+        return json.dumps(json_string)
 
     @classmethod
     def create(cls, **dictionary):
