ed45210cca8bbb087af9cbece7e355f90a89c411
ffelipegupe <1560@holbertonschool.com>
2020-07-14 00:44:49 +0000

    Update: uppercases

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,5 +4,5 @@ name VARCHAR(256),
 score INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "BOB", 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "GEORGE", 8);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
