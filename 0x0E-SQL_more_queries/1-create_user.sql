fcb3ce11bc53b1dc6a110e82048612579b25ea69
ffelipegupe <1560@holbertonschool.com>
2020-07-14 21:29:47 +0000

    Add: adding 1-create_user.sql

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Script that creates the MySQL server user user_0d_1.
+IF NOT EXISTS CREATE `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
+GRANT ALL PRIVILEGES ON *.* TO `user_0d_1`@`localhost`;
