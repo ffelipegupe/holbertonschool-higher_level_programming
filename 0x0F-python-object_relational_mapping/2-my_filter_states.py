a5697c8a864b8baf0b7907981feb5e45b463f1df
ffelipegupe <1560@holbertonschool.com>
2020-08-09 22:43:38 +0000

    Edit: editing function syntax

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
     c.execute("SELECT * FROM states WHERE name like '{:s}' ORDER BY \
-    id ASC".format(argv[4])
+    id ASC".format(argv[4]))
     [print(state) for state in c.fetchall()]
