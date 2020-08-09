6e74ba0cf5349c1c6bc4243addab5b3f1effacba
ffelipegupe <1560@holbertonschool.com>
2020-08-09 22:52:25 +0000

    Edit: function syntax

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -7,6 +7,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
+    c.execute("SELECT cities.id, cities.name, states.name \
+    FROM cities \
+    INNER JOIN states \
+    ON cities.state_id = states.id \
+    ORDER BY cities.id")
     [print(city) for city in c.fetchall()]
