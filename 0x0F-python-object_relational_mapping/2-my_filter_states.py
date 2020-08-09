761304ebd46c37e5161c778bfe60977c5b84d96d
ffelipegupe <1560@holbertonschool.com>
2020-08-09 22:46:34 +0000

    Edit: editing function syntax

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,6 +7,7 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    c.execute("SELECT * FROM states WHERE name like '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
+    c.execute("SELECT * \
+    FROM states \
+    WHERE BINARY name = '{}'".format(argv[4]))
     [print(state) for state in c.fetchall()]
