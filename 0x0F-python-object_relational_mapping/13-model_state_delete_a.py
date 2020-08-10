b57e85c4723d80b521a257ffd34551c373ec0ae6
ffelipegupe <1560@holbertonschool.com>
2020-08-10 00:48:54 +0000

    Add: adding task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" All states via SQLAlchemy """
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """ Function body """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).filter(State.name.contains('a')):
+        session.delete(state)
+    session.commit()
