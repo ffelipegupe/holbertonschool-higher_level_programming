549d2a5f0b0d605a29d56e7b5f23ce2fbb4fdd99
ffelipegupe <1560@holbertonschool.com>
2020-08-10 00:06:33 +0000

    Add: adding 7-model_state_fetch_all.py

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -5,7 +5,7 @@ from sys import argv
 from sqlalchemy import create_engine
 from model_state import Base, State
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     """ Function body """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(argv[1], argv[2], argv[3]))
