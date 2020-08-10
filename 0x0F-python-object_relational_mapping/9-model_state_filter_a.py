9dce042036efcdde040c21b3516276fa608585da
ffelipegupe <1560@holbertonschool.com>
2020-08-10 00:20:29 +0000

    Add: adding task 9 file

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" All states with a via SQLAlchemy """
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+if __name__ == "__main__":
+    """ Function body """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).order_by(State.id):
+        if "a" in state.name:
+            print("{}: {}".format(state.id, state.name))
+    session.close()
