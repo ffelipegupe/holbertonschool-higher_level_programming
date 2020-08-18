0ffa4c06fbb52a9b34d31a040d192ab865959a77
ffelipegupe <1560@holbertonschool.com>
2020-08-18 23:58:02 +0000

    Update: updating param

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL, displays the size of the response
-curl -sI "$1" | grep "Content-Lenght" | cut -d' ' -f 2
+curl -sI "$1" | grep "Content-Lenght" | cut -d " " -f 2
