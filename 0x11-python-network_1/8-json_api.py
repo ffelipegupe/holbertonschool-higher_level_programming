ef11aed051aca3da9c5ca72ad49e91c91f98d36a
ffelipegupe <1560@holbertonschool.com>
2020-08-25 01:31:02 +0000

    Add: adding task 8 file

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -7,17 +7,16 @@ import sys
 
 
 if __name__ == "__main__":
-    letter = ""
+    letter = ''
     if len(sys.argv) > 1:
         letter = sys.argv[1]
     payload = {'q': letter}
-
-    req = requests.post("http://0.0.0.0:500/search_user", data=payload)
+    req = requests.post('http://0.0.0.0:5000/search_user', data=payload)
     try:
         req_data = req.json()
         if not req_data:
             print("No result")
         else:
-            print("[{}] {}".format(req_data.get("id"), req_data.get("name")))
+            print("[{}] {}".format(req_data.get('id'), req_data.get('name')))
     except ValueError:
         print("Not a valid JSON")
