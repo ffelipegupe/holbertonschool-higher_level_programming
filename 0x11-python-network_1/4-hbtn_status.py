fd20b069be67c9d95c704861a181a7bfd1258098
ffelipegupe <1560@holbertonschool.com>
2020-08-25 00:48:35 +0000

    Add: adding task 4 file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    req = request.get("https://intranet.hbtn.io/status")
+    req = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(req.text)))
     print("\t- content: {}".format(req.text))
