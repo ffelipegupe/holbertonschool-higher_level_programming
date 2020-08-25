59a2779a390987198a1184532a1877571a5e9c29
ffelipegupe <1560@holbertonschool.com>
2020-08-25 00:57:06 +0000

    Add: adding task 5 file

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the value of the X-Request-Id
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers.get("X-Request-Id"))
