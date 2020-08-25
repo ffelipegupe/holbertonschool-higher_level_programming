aeb0a7ee351f0487a612889357425c86f18c90ca
ffelipegupe <1560@holbertonschool.com>
2020-08-25 01:49:27 +0000

    Add: adding task 10 file

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Python script that takes your Github credentials
+(username and password) and uses the Github API to display your id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    usr = sys.argv[1]
+    psw = sys.argv[2]
+    req = requests.get('https://api.github.com/user', aut=(usr, psw))
+    print(req.json().get('id)))
