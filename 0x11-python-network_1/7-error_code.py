0e6f993ed8551979128ada9e45dd134c4ae957b4
ffelipegupe <1560@holbertonschool.com>
2020-08-25 01:02:27 +0000

    Add: adding task 7 file

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    if req.status_code >= 500:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
