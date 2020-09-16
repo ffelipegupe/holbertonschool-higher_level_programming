ae9155cfe2be6c471693a4e2d9e5f4a5af3f2d05
ffelipegupe <1560@holbertonschool.com>
2020-09-16 01:07:04 +0000

    Add: adding task 10 file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 // JS Warm up
 
-let arg = parseInt(process.argv[2]);
-function factorial(n) {
-    return (n != 1) ? n * factorial(n - 1) : 1;
+const arg = parseInt(process.argv[2]);
+function factorial (n) {
+  return (n !== 1) ? n * factorial(n - 1) : 1;
 }
 console.log(factorial(arg));
