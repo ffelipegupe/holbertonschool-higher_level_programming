78c26ebb424c4591bd09225aeb0a5e6937656a9b
ffelipegupe <1560@holbertonschool.com>
2020-09-16 00:44:39 +0000

    Add: adding task 8 file

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// JS Warm up
+
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+} else if (num < 0) {
+  console.log('');
+} else {
+  console.log('Missing size');
+}
