1df5cf98ed7ddeae837fd1852feeea2b012227f6
ffelipegupe <1560@holbertonschool.com>
2020-09-16 00:00:02 +0000

    Add: adding task 2 file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 
 const args = process.argv.length;
 if (args === 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (args === 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else if (args > 3) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
