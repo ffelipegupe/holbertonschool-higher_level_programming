2f1f3e505c11ef2f3655ee538b023eb0c9be6beb
ffelipegupe <1560@holbertonschool.com>
2020-09-16 00:21:36 +0000

    Add: adding task 6 file

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,6 +2,6 @@
 // JS Warm up
 
 const arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < arr.lenght; i++) {
+for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
