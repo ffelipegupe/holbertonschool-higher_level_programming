73e4675a4c3f412fef01fe025caec4c4b2a2f947
ffelipegupe <1560@holbertonschool.com>
2020-09-16 00:36:18 +0000

    Add: adding task 7 file

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// JS Warm up
+
+const num = parseInt(process.argv[2]);
+if (num) {
+  for (let i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
