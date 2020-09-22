5fa3b5251278d7491c05fe64660ffd16b24861ca
ffelipegupe <1560@holbertonschool.com>
2020-09-22 23:57:42 +0000

    Add: adding file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fpath = require('fs');
+
+request(`${process.argv[2]}`, (err, response, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const bod = body;
+    fs.writeFile(`./${process.argv[3]}`, bod, 'utf8', (err) => {
+	    if (err) {
+        console.log(err);
+	    }
+    });
+  }
+});
