7ff0a6a9e6299ac0d3337ef8635232532c1f6838
ffelipegupe <1560@holbertonschool.com>
2020-09-22 23:17:36 +0000

    Add: adding file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,9 @@
 const fpath = require('fs');
 
 fpath.readFile(`${process.argv[2]}`, (err, data) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data.toString('utf8'));
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString('utf8'));
+  }
 });
